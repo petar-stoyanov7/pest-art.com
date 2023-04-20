d53cc3e05fefdbe013d66b8f5358553f96a0e25d
Petar Stoyanov <petar@ahorn-design.eu>
2023-04-20 12:34:49 +0300

    Random caricature;
    + Added random caricature page;
    + Modified caricatures template to support it;

---
+++ content/themes/pest-art/page-random.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * The Template file for displaying random caricature
+ *
+ * @package Pest-art.com
+ * @since Pest-art.com 0.0.1
+ */
+
+$randomQuery = new WP_Query([
+	'post_type'         => 'caricature',
+	'post_status'       => 'publish',
+	'posts_per_page'    => 1,
+	'orderby'           => 'rand',
+]);
+
+get_header();
+?>
+
+<main class="pa-content pa-single">
+	<?php
+	if ($randomQuery->have_posts()) {
+		while($randomQuery->have_posts()) {
+			$randomQuery->the_post();
+			get_template_part(
+				'template-parts/single',
+				get_post_type(),
+				['is-random' => true]
+			);
+		}
+	}
+	?>
+</main>
+
+<?php get_footer(); ?>
--- content/themes/pest-art/template-parts/single-caricature.php
+++ content/themes/pest-art/template-parts/single-caricature.php
@@ -6,19 +6,24 @@
  * @since Pest-art.com 0.0.1
  */
 
+$isRandom = !empty($args) && !empty($args['is-random']) && $args['is-random'];
 $previousClass = "ps-controls__control is-previous";
 $nextClass = "ps-controls__control is-next";
 
 $previous = get_previous_post();
-$previousClass .= empty($previous) ? " is-disabled" : '';
+$previousClass .= empty($previous) && !$isRandom ? " is-disabled" : '';
 $previousLink = empty($previous) ? '#' : get_permalink($previous->ID);
 
 $next = get_next_post();
-$nextClass .= empty($next) ? " is-disabled" : '';
+$nextClass .= empty($next) && !$isRandom ? " is-disabled" : '';
 $nextLink = empty($next) ? '#' : get_permalink($next->ID);
 
 $img = get_the_post_thumbnail_url();
 $title = get_the_title();
+
+if (!empty($args) && !empty($args['is-random']) && $args['is-random']) {
+    $nextLink = $previousLink = '/random';
+}
 ?>
 
 <ul class="ps-controls">
