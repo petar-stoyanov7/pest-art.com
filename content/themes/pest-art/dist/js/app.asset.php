7a92fe36a025c36261946de3fd339a46cc700d91
Petar Stoyanov <petar@ahorn-design.eu>
2023-04-27 13:43:14 +0300

    Updated dist

--- content/themes/pest-art/dist/js/app.asset.php
+++ content/themes/pest-art/dist/js/app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery'), 'version' => '7cd1200202e4c7eb5123');
+<?php return array('dependencies' => array('jquery'), 'version' => 'da0a25bd65e15660c0d8');
